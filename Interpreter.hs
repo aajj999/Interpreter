d320132147f55fae508e055774c994330c878c85
Anna <jablonowska999@gmail.com>
2022-05-14 14:04:09 +0200

    Displpay global variables

--- Interpreter.hs
+++ Interpreter.hs
@@ -8,9 +8,10 @@ import Data.Maybe
 import Distribution.Simple.Setup (trueArg)
 import Control.Monad.Except
 import Control.Monad.State
+import Data.Time.Clock.POSIX (systemToPOSIXTime)
 
 type Location = Int
-data Value = VInt Integer | VBool Bool | VString String
+data Value = VInt Integer | VBool Bool | VString String deriving Show
 type Variables = Map.Map Ident Location
 type Locations = Map.Map Location Value
 
@@ -28,33 +29,42 @@ umt = "Unmatching types"
 env :: Variables -> Locations -> Functions -> Env
 env var loc fun = (var, loc, fun)
 
+newLoc :: Locations -> Location
+newLoc l = if Map.null l then 0 else (do
+           let p = foldr1 (\x y ->if x >= y then x else y) $ Map.keys l
+           p + 1)
+
 interpret :: Program -> IO()
 interpret (Program _ globals topdefs) = do
-  evalState (transGlobals globals) (env Map.empty Map.empty)
-  put $ env Map.empty Map.empty
-  transGlobals globals
-  transTopDefs topdefs
+  let e = execState (transGlobals globals) (env Map.empty Map.empty Map.empty)
+  evalState (transTopDefs topdefs) e
 
-transGlobals :: [Global] -> Env -> Env
-transGlobals [] = print ""
+transGlobals :: [Global] -> State Env ()
+transGlobals [] = return()
 transGlobals (x : xs) = do
   transGlobal x
   transGlobals xs
 
-transGlobal :: Global -> ()
-transGlobal x  = case x of
-  GlobalDef _ t id expr -> v' where
-    v' = case transExpr expr v of
-      Nothing -> v
-      Just val -> do
-        Map.insert id val 
+transGlobal :: Global -> State Env ()
+transGlobal x = case x of
+  GlobalDef _ t id expr -> do
+    let val = transExpr expr
+    case val of
+      Left _ -> return()
+      Right val -> do
+        (var, loc, fun) <- get
+        let l = newLoc loc
+        let var' = Map.insert id l var
+        let loc' = Map.insert l val loc
+        put (var', loc', fun)
 
-addVariable :: Type -> Ident -> Value -> Variables -> Variables
-addVariable t id x v = Map.insert id x v
-
-
-transTopDefs :: [TopDef] -> IO()
-transTopDefs [] _ = putStrLn ""
+transTopDefs :: [TopDef] -> State Env (IO ())
+transTopDefs [] = return $ putStrLn ""
+transTopDefs (x : xs) = do
+  (var, loc, fun) <- get
+  put(var, loc, fun)
+  return $ print (var, loc)
+{-transTopDefs [] _ = putStrLn ""
 transTopDefs (x : xs)  v = case x of
   FnDef _a _type _ident _args _block -> do
     transTopDef x v
@@ -85,7 +95,10 @@ transArg x = case x of
 
 transBlock :: Block -> Env -> IO()
 transBlock x v = case x of
-  Block _ stmts -> err
+  Block _ stmts -> do
+    save env
+    transStmt stmts
+    put odl env
 
 transStmt :: Show a => Stmt' a -> IO()
 transStmt x = case x of
@@ -100,33 +113,27 @@ transStmt x = case x of
   CondElse _ expr stmt1 stmt2 -> err
   While _ expr stmt -> err
   SExp _ expr -> err
-  Print _ expr -> err
+  Print _ expr -> putStrLn expr
 
-transItem :: Show a => Item' a -> IO()
-transItem x = case x of
-  NoInit _ ident -> err
-  Init _ ident expr -> err
-
-transType :: Show a => Type' a -> IO()
-transType x = case x of
-  Int _ -> err
-  Str _ -> err
-  Bool _ -> err
-  Fun _ type_ types -> err
+transItem :: Type -> Item -> IO()
+transItem t x = do
+  (var, loc, fun) <- ask
+  case x of
+    NoInit _ ident -> do
+      l <- newLoc
+      var' <- Map.insert ident l var
+      {-case t of
+        Str -> loc' <- Map.insert l "" loc
+        Int -> loc' <- Map.insert l 0  loc
+        Bool -> loc' <- Map.insert l False loc-}
+      put (var', loc', fun)
+    Init _ ident expr -> do
+      var' <- Map.insert ident l var
+      loc' <- Map.insert l transExpr expr loc
+      put(var', loc', fun)-}
 
 transExpr :: Expr -> Either [Char] Value
 transExpr x = case x of
-  EVar _ ident -> do
-    (var, loc, _) <- ask
-    l <- Map.lookup ident var
-    case l of
-      Nothing -> throwError "Not existing variable used"
-      Just lo -> do
-        x <- Map.lookup l loc
-        case x of
-          Nothing -> throwError "Not existing variable used"
-          Just xv -> xv 
-  EApp _ ident exprs -> Nothing   
   AbsAJ.ELitInt _ integer -> Right $ VInt integer
   AbsAJ.ELitTrue _ -> Right $ VBool True
   AbsAJ.ELitFalse _ -> Right $ VBool False
@@ -151,6 +158,18 @@ transExpr x = case x of
     _ -> case transExpr expr2 of
       Right (VBool True) -> Right $ VBool True
       _ -> Right $ VBool True
+  _ -> Right $ VString "aaa"
+  {-EVar _ ident -> do
+    (var, loc, _) <- ask
+    l <- Map.lookup ident var
+    case l of
+      Nothing -> throwError "Not existing variable used"
+      Just lo -> do
+        x <- Map.lookup l loc
+        case x of
+          Nothing -> throwError "Not existing variable used"
+          Just xv -> xv 
+  EApp _ ident exprs -> Nothing  -}
 
 transAddOp :: AddOp -> Expr -> Expr -> Either [Char] Value
 transAddOp x a b = case transExpr a of
@@ -161,7 +180,7 @@ transAddOp x a b = case transExpr a of
     _ -> throwError umt
   Right (VString as) -> case transExpr b of
     Right (VString bs) -> case x of
-      Plus _ -> Right $ VString $ as + bs
+      Plus _ -> Right $ VString $ as ++ bs
       _ -> throwError "Not allowed opeartion on strings: -"
     _ -> throwError umt
   _ -> throwError $ "Using bool in opeartion" ++ show x
@@ -175,16 +194,29 @@ transMulOp x a b = case transExpr a of
       Mod p -> throwError $ "Not allowed opearation mod with integer 0" ++ show p
     Right (VInt bv) -> case x of
       Times _ -> Right $ VInt $ av * bv
-      Div _ -> Right $ VInt $ av / bv
+      Div _ -> Right $ VInt $ div av bv
       Mod _ -> Right $ VInt $ mod av bv
     _ -> throwError umt
   _ -> throwError "Wrong variable type for operation"
 
 transRelOp :: RelOp -> Expr -> Expr -> Value
-transRelOp x a b = case x of
-  LTH _ -> VBool $ transExpr a < transExpr b
-  LE _ -> VBool $ transExpr a <= transExpr b
-  GTH _ -> VBool $ transExpr a > transExpr b
-  GE _ -> VBool $ transExpr a >= transExpr b
-  EQU _ -> VBool $ transExpr a == transExpr b
-  NE _ -> VBool $ transExpr a /= transExpr b
+transRelOp x a b = case transExpr a of
+  Right (VInt va) -> case transExpr b of
+    Right (VInt vb) -> case x of
+      LTH _ -> VBool $ va < vb
+      LE _ -> VBool $  va <= vb
+      GTH _ -> VBool $  va > vb
+      GE _ -> VBool $  va >=  vb
+      EQU _ -> VBool $  va ==  vb
+      NE _ -> VBool $  va /=  vb
+    _ -> VBool False
+  Right (VString sa) -> case transExpr b of
+    Right (VString sb) -> case x of
+      LTH _ -> VBool $  sa <  sb
+      LE _ -> VBool $  sa <= sb
+      GTH _ -> VBool $  sa > sb
+      GE _ -> VBool $  sa >=  sb
+      EQU _ -> VBool $  sa ==  sb
+      NE _ -> VBool $  sa /=  sb
+    _ -> VBool False
+  _ -> VBool False
